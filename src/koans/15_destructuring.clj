d967fb5c78ae28ffbcdb52b8160f253a04b0c5f3
Agata Sumowska <agata.sumowska@gmail.com>
2020-06-10 23:13:02 +0200

    start deconstructing

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -1,5 +1,6 @@
 (ns koans.15-destructuring
-  (:require [koan-engine.core :refer :all]))
+  (:require [koan-engine.core :refer :all]
+  [clojure.string :as string]))
 
 (def test-address
   {:street-address "123 Test Lane"
@@ -8,21 +9,30 @@
 
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
          [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "An Oxford comma list of apples, "
           "oranges, "
           "and pears.")
-     ((fn [[a b c]] __)
+     ((fn [[a b c]] (str "An Oxford comma list of " (str a) ", " (str b) ", and " (str c) "." ))
       ["apples" "oranges" "pears"]))
+
+   "You can start somewhere else"
+  (= 2400 (reduce (fn [a b] (* a b)) 100 [1 2 3 4]))
+
+  "1"
+  (= " aka The Clojurer aka Go Time aka Lambda Guru"
+     (let [[first-name last-name & aliases]
+           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
+       (reduce (fn [a b] (str (str a) " aka " (str b))) "" aliases)))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       __))
+       (str first-name " " last-name (str (reduce (fn [a b] (str (str a) " aka " (str b))) "" aliases)))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
